0db76369135d86308e061c74bd7867f6d058d03e
Pirasis Leelatanon <pirasis.leelatanon@thomsonreuters.com>
2016-05-09 13:59:02 +0700

    export server as function

--- modules/server.js
+++ modules/server.js
@@ -8,47 +8,49 @@
   vars-on-top
 */
 
-var _ = require('lodash');
-var http = require('http');
+module.exports = function () {
+  var _ = require('lodash');
+  var http = require('http');
 
-var server = new http.Server();
-var io = require('socket.io')(server);
-var port = 3000;
+  var server = new http.Server();
+  var io = require('socket.io')(server);
+  var port = 3000;
 
-server.listen(port);
+  server.listen(port);
 
-var events = [
-  'download',
-  'context-change',
-  'quotes-reset',
-  'quotes-create',
-  'quotes-rics',
-  'quotes-chain',
-  'quotes-filter',
-  'quotes-rawFields',
-  'quotes-formattedFields',
-  'quotes-start',
-  'quotes-stop',
-  'quotes-onNewRow',
-  'quotes-onUpdate',
-  'quotes-onRemoveRow',
-  'settings-read',
-  'settings-read-success',
-  'navigate',
-  'publish',
-  'subscribe',
-  'subscribe-success',
-  'udf-request',
-  'udf-response',
-  'proxy-request-get',
-  'proxy-request-post',
-  'proxy-response',
-];
+  var events = [
+    'download',
+    'context-change',
+    'quotes-reset',
+    'quotes-create',
+    'quotes-rics',
+    'quotes-chain',
+    'quotes-filter',
+    'quotes-rawFields',
+    'quotes-formattedFields',
+    'quotes-start',
+    'quotes-stop',
+    'quotes-onNewRow',
+    'quotes-onUpdate',
+    'quotes-onRemoveRow',
+    'settings-read',
+    'settings-read-success',
+    'navigate',
+    'publish',
+    'subscribe',
+    'subscribe-success',
+    'udf-request',
+    'udf-response',
+    'proxy-request-get',
+    'proxy-request-post',
+    'proxy-response',
+  ];
 
-io.on('connection', function (socket) {
-  _.forEach(events, function (e) {
-    socket.on(e, function (p1, p2, p3, p4) {
-      socket.broadcast.emit(e, p1, p2, p3, p4);
+  io.on('connection', function (socket) {
+    _.forEach(events, function (e) {
+      socket.on(e, function (p1, p2, p3, p4) {
+        socket.broadcast.emit(e, p1, p2, p3, p4);
+      });
     });
   });
-});
+}
