98d9429e77473b65d33b907bfb5c1b29a3d7bc76
Pirasis Leelatanon <pirasis.leelatanon@thomsonreuters.com>
2016-07-08 07:51:21 +0700

    extend webpack production config from base config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,35 +1,14 @@
-const path = require('path');
 const webpack = require('webpack');
+let config = require('./webpack.config.js');
 
-module.exports = {
-  context: __dirname,
-  entry: './app/main.js',
-  output: {
-    path: path.join(__dirname, 'dist'),
-    publicPath: '/',
-    filename: 'bundle.js',
-  },
-  recordsOutputPath: path.join(__dirname, 'records.json'),
-  module: {
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules|bower_components/, loader: 'babel-loader' },
-      { test: /\.scss$/, loaders: ['style', 'css', 'sass'] },
-      { test: /\.(eot|svg|ttf|woff|woff2)/, loader: 'file?name=public/fonts/[name].[ext]' },
-      { test: /\.(png|jpg)$/, loader: 'url?limit=8192' },
-    ],
-  },
-  devtool: 'source-map',
-  devServer: {
-    contentBase: './dist',
-    hot: true,
-  },
-  plugins: [
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false,
-        screw_ie8: true,
-      },
-    }),
-  ],
-};
+config.plugins = [
+  new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+  new webpack.optimize.UglifyJsPlugin({
+    compressor: {
+      warnings: false,
+      screw_ie8: true,
+    },
+  }),
+];
+
+module.exports = config;
