40d911c73597b674b2bcded8c9ed8d09e4e9cc64
Pirasis Leelatanon <pirasis.leelatanon@thomsonreuters.com>
2016-07-10 21:52:25 +0700

    [route] sync with top iframe

--- app/route.js
+++ app/route.js
@@ -7,11 +7,12 @@ const route = {
   init() {
     $content = $('.wrapper > .content-wrapper');
     $sidebar = $('.sidebar-menu');
-    const currentLocation = location.hash;
+    const currentLocation = window.top.location.hash;
     let $node = $sidebar.find('a').first();
     $sidebar.find('a').each((i, el) => {
-      if ($(el).attr('href') === currentLocation) {
-        $node = $(el);
+      let $el = $(el);
+      if ($el.attr('href') === currentLocation) {
+        $node = $el;
         return false;
       }
       return true;
@@ -27,6 +28,9 @@ const route = {
     $node.parent().addClass('active');
     let name = $node.attr('href').slice(2);
     $content.attr('show', name);
+    if (window.top !== window) {
+      window.top.location.hash = `/${name}`;
+    }
   },
 };
 
