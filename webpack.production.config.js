e4d8ab8091ed162e0d0b2a01f5609a64a41ed7db
Pirasis Leelatanon <pirasis.leelatanon@thomsonreuters.com>
2016-07-11 18:15:31 +0700

    [webpack] fix prod public path

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 let config = require('./webpack.config.js');
 
+config.output.publicPath = '';
 config.devtool = 'source-map';
 config.plugins = [
   new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
