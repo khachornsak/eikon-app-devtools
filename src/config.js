8ee62a8d447fecb554aead8e4450234857fa1c8b
Pirasis Leelatanon <pirasis.leelatanon@thomsonreuters.com>
2017-01-06 14:43:55 +0700

    Revert "add `pem`"
    This reverts commit 2f96932e8ea847a441260bb0238d53f259d756f6.

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 module.exports = {
   defaultPort: 3000,
-  defaultSslPort: 3001,
   defaultSocketUrl: 'http://localhost:3000',
 }
--- src/server.js
+++ src/server.js
@@ -1,7 +1,5 @@
 const chalk = require('chalk')
 const http = require('http')
-const https = require('https')
-const pem = require('pem')
 const socketio = require('socket.io')
 
 const config = require('./config')
@@ -35,35 +33,25 @@ const events = [
 ]
 
 module.exports = (options = {}) => {
-  let { port, sslPort } = options
+  let server = new http.Server()
+  let io = socketio(server)
+  let port = options.port
   if (!Number.isInteger(port)) port = config.defaultPort
-  if (!Number.isInteger(sslPort)) sslPort = config.defaultSslPort
 
-  pem.createCertificate({ days: 3, selfSigned: true }, (err, keys) => {
-    let servers = [
-      [port, new http.Server()],
-      [sslPort, https.createServer({ key: keys.serviceKey, cert: keys.certificate })],
-    ]
+  server.listen(port)
+  server.on('error', (e) => {
+    if (e.code === 'EADDRINUSE') {
+      console.error(chalk.red('EAD: another instance of EAD may be currently in use'))
+      return
+    }
 
-    servers.forEach(([p, server]) => {
-      let io = socketio(server)
+    throw e
+  })
 
-      server.listen(p)
-      server.on('error', (e) => {
-        if (e.code === 'EADDRINUSE') {
-          console.error(chalk.red('EAD: another instance of EAD may be currently in use'))
-          return
-        }
-
-        throw e
-      })
-
-      io.on('connection', (socket) => {
-        events.forEach((e) => {
-          socket.on(e, (...args) => {
-            socket.broadcast.emit(e, ...args)
-          })
-        })
+  io.on('connection', (socket) => {
+    events.forEach((e) => {
+      socket.on(e, (...args) => {
+        socket.broadcast.emit(e, ...args)
       })
     })
   })
